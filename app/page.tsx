c3e4a4fa2ace1816a94ac056921cb22f7dfabbbb
Omar <omar.abdesslem@etu.unige.ch>
2025-07-04 17:33:53 +0200

    Update root page for new folder structure

--- app/page.tsx
+++ app/page.tsx
@@ -26,11 +26,11 @@ export default function Page(): JSX.Element {
         {/* Model Navigation */}
         <nav className="text-sm mb-2 sm:mb-3 md:mb-4">
           <ul className="flex flex-wrap justify-center space-x-4">
-            <li><a href="/AR" className="text-blue-900 hover:underline">AR</a></li>
-            <li><a href="/ARMA" className="text-blue-900 hover:underline">ARMA</a></li>
-            <li><a href="/ARIMA" className="text-orange-900 hover:underline">ARIMA</a></li>
-            <li><a href="/SARIMA" className="text-blue-900 hover:underline">SARIMA</a></li>
-            <li><a href="/SARIMAX" className="text-blue-900 hover:underline">SARIMAX</a></li>
+            <li><a href="/Model_Pages/AR" className="text-blue-900 hover:underline">AR</a></li>
+            <li><a href="/Model_Pages/ARMA" className="text-blue-900 hover:underline">ARMA</a></li>
+            <li><a href="/Model_Pages/ARIMA" className="text-orange-900 hover:underline">ARIMA</a></li>
+            <li><a href="/Model_Pages/SARIMA" className="text-blue-900 hover:underline">SARIMA</a></li>
+            <li><a href="/Model_Pages/SARIMAX" className="text-blue-900 hover:underline">SARIMAX</a></li>
           </ul>
         </nav>
 
@@ -48,7 +48,7 @@ export default function Page(): JSX.Element {
       {/* Code Button */}
 <div className="flex flex-wrap justify-center gap-1 sm:gap-2 mt-2 sm:mt-4 mb-4 sm:mb-5 text-sm">
     <a
-    href="/I._Overview"
+    href="/Code_Pages/I._Overview"
     rel="noopener noreferrer"
     className="bg-gray-800 hover:bg-gray-700 py-1 px-2 sm:py-1.5 sm:px-3 rounded"
   >
@@ -68,7 +68,7 @@ export default function Page(): JSX.Element {
 
       {/* Footer */}
       <footer className="text-xs text-gray-400 py-1.5 sm:py-3 text-center w-full border-t border-gray-700 -mt-1">
-  <p>© 2025 <a href="https://omar-abdesslem.ch" target="_blank" className="hover:underline hover:text-white">Omar Abdesslem</a></p>
+  <p>© 2025 Omar Abdesslem</p>
         <p className="mt-1">
           Powered by <a href="https://www.swissgrid.ch" target="_blank" className="underline hover:text-white">Swissgrid</a> Data, visualized with <a href="https://plotly.com/javascript/" target="_blank" className="underline hover:text-white">Plotly</a>, inspired by <a href="https://github.com/arthurgassner" target="_blank" className="underline hover:text-white">Arthur Gassner</a>.
         </p>
